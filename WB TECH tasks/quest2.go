3775ac250f2228e7d41d86e94e31d77d2d187234
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-07-09 18:59:43 +0300

    Update quest2.go

--- WB TECH tasks/quest2.go
+++ WB TECH tasks/quest2.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func Number(s string) int {
 	runes := []rune(s)
@@ -12,9 +15,14 @@ func Number(s string) int {
 }
 
 func main(){
+	wg:=&sync.WaitGroup{}
 	mas:=[...] string{"abcdefghijklmnopqrstuvwxyz","abcdefghijklmnopqrstuvwxy","vccc"}
 for i,v:= range mas{
-	num:=Number(v)
+	wg.Add(1)
+	go func(i int,v string){
+		defer wg.Done()
+		num:=Number(v)
 	fmt.Printf("Количество уникальных символов у строки на позиции %d: %d\n",i,num)
-}
+}(i,v)}
+wg.Wait()
 }
