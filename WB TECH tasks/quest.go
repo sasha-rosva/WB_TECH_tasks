e1192abb003cd1b9d17543acf893954fe24493d0
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-07-09 18:45:14 +0300

    Update quest.go

--- WB TECH tasks/quest.go
+++ WB TECH tasks/quest.go
@@ -8,16 +8,20 @@ import (
 
 func main(){
 	mu:=sync.RWMutex{}
+	wg:=&sync.WaitGroup{}
 	map1:=make(map[rune]int)
 	str:="AAABBCCCDDEEE"
 	runes := []rune(str)
 	for _,v:=range runes{
+		wg.Add(1)
 		go func(v rune){
+			defer wg.Done()
 			mu.Lock() // Блокировка
 			map1[v]++
 			mu.Unlock() // Отключение блокировки
 		}(v)
 	}
+	wg.Wait()
 	var ourStr string
 	sb:=new(strings.Builder)
 	for ii,vv:= range map1 {
