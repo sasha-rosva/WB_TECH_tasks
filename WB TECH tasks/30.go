15d37cca156f460ffb079419d93ff5ed5f579179
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-07-09 15:36:19 +0300

    Update 30.go

--- WB TECH tasks/30.go
+++ WB TECH tasks/30.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func DeleteAnyValue(val []int, i int) []int {
-	if i>len(val)-1 {fmt.Println("В данном срезе нет такого индекса!")
+	if i>len(val)-1 || i<0{fmt.Println("В данном срезе нет такого индекса!")
 	return nil}
 	if len(val)==i+1{
 		val=val[:i]
@@ -17,7 +17,7 @@ func main(){
 	// Инициализируем срез
 	val:=[]int{1,2,3,4,5,6,7,8}
 	// Выбираем i-й элемент из слайса.
-	n:=4
+	n:=-9
 	newVal:=DeleteAnyValue(val,n)
 	// Выводим результат
 	if newVal!=nil{fmt.Println(newVal)}
