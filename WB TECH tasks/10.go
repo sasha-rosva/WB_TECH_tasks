4362d237fbece695da8ec24693df425630dd437f
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-07-09 17:59:58 +0300

    Update 10.go

--- WB TECH tasks/10.go
+++ WB TECH tasks/10.go
@@ -4,28 +4,15 @@ import "fmt"
 
 func main (){
 	ourMap:=make(map[int] []float32)
-	mas:=[...]float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	mas:=[...]float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5, -13.8}
+	var intValue int
 	for _,v:= range mas{
-		switch {
-		case v<=-20 && v>-30:
-			ourMap[-20]=append(ourMap[-20],v)
-		case v<=-10 && v>-20:
-			ourMap[-10]=append(ourMap[-10],v)
-		case v<=0 && v>-10:
-			ourMap[0]=append(ourMap[0],v)
-		case v<=10 && v>0:
-			ourMap[10]=append(ourMap[10],v)
-		case v<=20 && v>10:
-			ourMap[20]=append(ourMap[20],v)
-		case v<=30 && v>20:
-			ourMap[30]=append(ourMap[30],v)
-		case v<=40 && v>30:
-			ourMap[40]=append(ourMap[40],v)
-		}
-		}
-		fmt.Println(ourMap)
-
+		intValue=int(v/10)
+		ourMap[10*intValue]=append(ourMap[10*intValue],v)
 	}
+	fmt.Println(ourMap)
+
+}
 
 
 
