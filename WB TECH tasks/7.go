e97cd0c00ae06ce366dc2481ac0a9235788e8f6e
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-07-09 14:10:34 +0300

    Update 7.go

--- WB TECH tasks/7.go
+++ WB TECH tasks/7.go
@@ -8,9 +8,10 @@ import (
 func main(){
 	// Map является конкурентно не безопасным типом данных,
 	// поэтому воспользуемся sync.Mutex для надежного обращения с map.
-	mu:=&sync.Mutex{}
+	mu:=sync.RWMutex{}
 	map1:=make(map[int]int)
 	mas:=[...]int{0,1,2,3,4,5,6,7,8,9}
+	n:=len(mas)
 	for i,v:=range mas{
 		go func(i,v int){
 			mu.Lock() // Блокировка
@@ -18,7 +19,11 @@ func main(){
 			mu.Unlock() // Отключение блокировки
 		}(i,v)
 	}
-	mu.Lock()
-fmt.Println(map1[2])
-	mu.Unlock()
+	// Ждем оканчания записи в map1
+	for{
+		if len(map1)==n{break}
+	}
+	mu.RLock()
+fmt.Println(map1[3])
+	mu.RUnlock()
 }
