703d8a41544e4f57fbd0bcbbca2bf8a18c644a7b
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-07-09 13:54:03 +0300

    Update 6.1.go

--- WB TECH tasks/6.1.go
+++ WB TECH tasks/6.1.go
@@ -3,26 +3,33 @@ package main
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"time"
 )
 // Функция worker, запущенная в горутине, закрывается при получения сигнала от контекста
-func worker(ctx context.Context, num int,out chan<- int){
-	select{
+func worker(ctx context.Context, num int){
+	for {select{
 	case <-ctx.Done(): // ждем сигнал о закрытии
+		fmt.Println("Worker ",num," закрылся!")
 		return
-		case <-time.After(3*time.Second):
-			out<-num
+	case <-time.After(time.Second):
+		fmt.Println("Worker ",num)
 	}
-}
+	}}
 
 func main(){
-	ch1:=make(chan int,1)
 	ctx,finish:=context.WithCancel(context.Background())
-for i:=0;i<10;i++{
-	go worker(ctx,i,ch1)
-}
+	for i:=0;i<10;i++{
+		go worker(ctx,i)
+	}
 
-fmt.Println(<-ch1)
-// Отправляем сигнал в контекст о закрытии
-finish()
+	time.Sleep(3*time.Second)
+
+	// Отправляем сигнал в контекст о закрытии
+	finish()
+	// Ждем пока все горутины не закроются
+	for {
+		if runtime.NumGoroutine()==1{break}
+	}
+	fmt.Println("Все горутины успешно закрылись!")
 }
