4c20bbb82f2c162b7774081c86902f947c4fae28
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-07-09 15:24:08 +0300

    Update 24.go

--- WB TECH tasks/24.go
+++ WB TECH tasks/24.go
@@ -3,18 +3,7 @@ package main
 import "fmt"
 
 func main(){
-// Создаем слайс с емкостью 2. Это означает, что в слайс можно добавить 2 значения без аллоцирования новой памяти.
-	// При переполнения емкости, срез будет ссылаться на бОльший массив с копированием данных в него
-	slice:=make([]int,0,2)
-
-	slice=append(slice,88)
-	fmt.Println(slice,cap(slice))
-	slice=append(slice,88)
-	fmt.Println(slice,cap(slice))
-	slice=append(slice,88)
-	fmt.Println(slice,cap(slice))
-	slice=append(slice,88)
-	fmt.Println(slice,cap(slice))
-	slice=append(slice,88)
-	fmt.Println(slice,cap(slice))
+	slice:=make([]int,100)
+	
+	fmt.Printf("Наш слайс: %v\nЁмкость нашего слайса: %v",slice,cap(slice))
 }
